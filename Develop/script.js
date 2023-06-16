84eb0b32cacf8e0fd20fbd24e2d9d743d7bdafdc
Nathan Moon <moonman700@gmail.com>
2023-06-16 14:27:50 -0400

    added password generation logic

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,8 @@
 // Assignment code here
-var alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var specialCharacters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "\\","|", ";", ":", "'", "\"", ",", "<", ".", ">", "/", "?"]
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+const alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const specialCharacters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "\\","|", ";", ":", "'", "\"", ",", "<", ".", ">", "/", "?"]
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
 function generatePassword() {
   let password = ""
@@ -12,6 +12,7 @@ function generatePassword() {
   let passwordUpper = confirm("Would you like to include uppercase letters?")
   let passwordSpecial = confirm("Would you like to include special characters?")
   let passwordNumbers = confirm ("Would you like to include numbers?")
+
   if (passwordLower == true) {
     passwordArray = passwordArray.concat(alphabetLower)
   }
@@ -24,7 +25,9 @@ function generatePassword() {
   if (passwordNumbers == true) {
     passwordArray = passwordArray.concat(numbers)
   }
-  console.log(passwordArray)
+  for (let i = 0; i < passwordLength; i++) {
+    password = password + passwordArray[Math.floor(Math.random() * (passwordArray.length + 1))]
+  }
   
   return password
 }
