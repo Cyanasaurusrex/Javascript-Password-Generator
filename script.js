ba1503383553b7a35f5dd5594d60512b118ef16f
Nathan Moon <moonman700@gmail.com>
2023-06-16 15:12:52 -0400

    added a README.md as well as comments throughout code

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-// Assignment code here
+// Constant arrays of accepted characters
 const alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 const alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 const specialCharacters = ["`", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "\\","|", ";", ":", "'", "\"", ",", "<", ".", ">", "/", "?"]
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
+// Function for generating password
 function generatePassword() {
+
+  // Empty password string and array for password generation
   let password = ""
   let passwordArray = []
+
+  // Prompts user for password characteristics
   let passwordLength = prompt("How long would you like your password to be?")
   let passwordLower = confirm("Would you like to include lowercase letters?")
   let passwordUpper = confirm("Would you like to include uppercase letters?")
   let passwordSpecial = confirm("Would you like to include special characters?")
   let passwordNumbers = confirm ("Would you like to include numbers?")
 
+  // Logic for adding selected characters to password characters
   if (passwordLower == true) {
     passwordArray = passwordArray.concat(alphabetLower)
   }
@@ -25,10 +31,13 @@ function generatePassword() {
   if (passwordNumbers == true) {
     passwordArray = passwordArray.concat(numbers)
   }
+
+  // Cycles through password, adding a random character each cycle
   for (let i = 0; i < passwordLength; i++) {
     password = password + passwordArray[Math.floor(Math.random() * (passwordArray.length + 1))]
   }
   
+  // returns password to be displayed
   return password
 }
 
